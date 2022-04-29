1407cf19c2f85993cded5f2b15df152932184fd4
satyamraj <satyamraj-creator@users.noreply.github.com>
2022-04-29 18:40:10 +0530

    first mvc controller

---
+++ src/main/java/todolist/LoginController.java
@@ -0,0 +1,14 @@
+package todolist;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginController {
+@RequestMapping(value="/login")
+@ResponseBody
+public String sayHello() {
+	return "Helllo";
+}
+}
