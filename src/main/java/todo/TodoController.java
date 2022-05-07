4fc0b89a858cea5e014df83edb34745edbea75a5
satyamraj <satyamraj-creator@users.noreply.github.com>
2022-05-07 17:50:35 +0530

    exception handling

--- src/main/java/todo/TodoController.java
+++ src/main/java/todo/TodoController.java
@@ -3,8 +3,11 @@ package todo;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,15 +16,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import exception.ExceptionController;
+
 @Controller
 @SessionAttributes("name")
 public class TodoController {
+
+private Log logger = LogFactory.getLog(ExceptionController.class);
 
 @Autowired
 TodoService service;
@@ -30,6 +38,12 @@ TodoService service;
 protected void initBinder(WebDataBinder binder) {
 	SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
 	binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, false));
+}
+
+@ExceptionHandler(value = Exception.class)
+public String handleError(HttpServletRequest req, Exception exception) {
+	logger.error("Request: " + req.getRequestURL() + " raised " + exception);
+	return "common/specific-error";
 }
 
 @RequestMapping(value="/list-todos", method=RequestMethod.GET)
@@ -41,9 +55,9 @@ public String litsTodo(ModelMap model) {
 
 @RequestMapping(value="/add-todo", method=RequestMethod.GET)
 public String addTodoForm(ModelMap model) {
-	throw new RuntimeException("Dummy error");
-//	model.addAttribute("todo",new Todo(0,getLoggedInUser(),"Description here",new Date(),false));
-//	return "add-todo";
+//	throw new RuntimeException("Dummy error");
+	model.addAttribute("todo",new Todo(0,getLoggedInUser(),"Description here",new Date(),false));
+	return "add-todo";
 }
 
 @RequestMapping(value="/add-todo", method=RequestMethod.POST)
